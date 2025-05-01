a618dd82ff7426b27614f4c301bb451b5b366b62
Pavani <pavanimunasa@gmail.com>
2025-05-01 22:23:38 +0530

    Add components folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import SignupPage from "./components/SignupPage";
+import SigninPage from "./components/Signin";
+import AccountSettings from "./components/AccountSettings";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/account" element={<AccountSettings />} />
+      </Routes>
+    </Router>
   );
 }
 
